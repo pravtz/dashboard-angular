1b0e1893755a463466edc02318339bbf6b92bc81
pravtz <ederson.ufrgs@gmail.com>
2019-10-26 21:41:22 -0300

    implementado uma requisição http da API citada no codigo como exemplo

--- src/app/resuso/resuso.component.ts
+++ src/app/resuso/resuso.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-resuso',
@@ -7,27 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResusoComponent implements OnInit {
 
+  resumo; // será carregado a partir da API http://www.devup.com.br/php/api-dashboard/api/resumo
 
-  resumo = {
-    consultas: {
-      consultas_30dias_anteiores: 87,
-      consultas_30dias_posteriores: 79
-    },
-    faturamento: {
-      anterior: {
-        valor: 100000,
-        comparativo: 19
-      },
-      previsao: {
-        valor: 90000,
-        comparativo: -10
-      }
-    }
-  }
+  // resumo = {
+  //   consultas: {
+  //     consultas_30dias_anteiores: 87,
+  //     consultas_30dias_posteriores: 79
+  //   },
+  //   faturamento: {
+  //     anterior: {
+  //       valor: 100000,
+  //       comparativo: 19
+  //     },
+  //     previsao: {
+  //       valor: 90000,
+  //       comparativo: -10
+  //     }
+  //   }
+  // }
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.http.get("http://www.devup.com.br/php/api-dashboard/api/resumo")
+      .subscribe( dados => this.resumo = dados);
+
+
   }
 
 }
